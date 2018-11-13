f1910205e4329574f2f56fc933ec40094d594eef
james <jamesdanielv@msn.com>
2018-11-12 18:22:43 -0800

    Update tinySPI.cpp

--- src/tinySPI.cpp
+++ src/tinySPI.cpp
@@ -6,6 +6,7 @@
 //
 // https://github.com/JChristensen/tinySPI
 // Jack Christensen 24Oct2013
+//includeded minor enhancements James Villeneuve
 #define speedOverSize true //if true faster performance however it uses 24 more bytes
 #define IdoNotUseInterrupts true //if you do no use interrups specifically USI, set this to true to save 10 bytes!
 #include <tinySPI.h>
